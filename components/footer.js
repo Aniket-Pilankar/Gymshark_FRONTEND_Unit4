9da8afbd8abfe428a50b5aed4764ebe405a77233
ritesh-2124 <riteshyad222000@gmail.com>
2022-01-20 02:07:49 +0530

    landing page completed successfully

---
+++ components/footer.js
@@ -0,0 +1,82 @@
+function footer(){
+    return ` <div id="top_footer_div">
+    <div id="for_catag">
+      <div>
+          <h3 class="heading_drop"> HELP</h3>
+          <a href="">FAQ</a>
+          <a href="">Delivery Information</a>
+          <a href="">Returns Policy</a>
+          <a href="">Make A Return</a>
+          <a href="">Orders</a>
+          <a href="">Submit a Fake</a>
+      </div>
+      <div>
+          <h3  class="heading_drop">MY ACCOUNT</h3>
+          <a href="">Login</a>
+          <a href="">Register</a>
+         </div>
+      <div>     
+          <h3  class="heading_drop">PAGES</h3>
+          <a href="">Gymshark Central</a>
+          <a href="">About Us</a> 
+          <a href="">Careers</a>
+          <a href="">Student Beans</a>
+          <a href="">Transparency Report</a>
+           </div>
+    </div>
+   
+    <div id="img_massage">
+           <h2>MORE ABOUT GYMSHARK</h2>  
+    
+    <div id="img_div">
+        <div>
+            <img src="https://images.ctfassets.net/wl6q2in9o7k3/4QtcnsrjM61MBOfKzadnle/a18d71a24ca40bbc8137b754927e9c75/blog.jpg" alt=""  height="80%"
+            width="100%">
+            <h5>BLOG</h5>
+        </div>
+        <div> <img src="//images.ctfassets.net/wl6q2in9o7k3/78i6cwhCg8cVO59Dvlk7Gy/ec15b2ca715a0b1aa27be0fd09c6aa03/newsletter__1_.jpg" alt="" height="80%"
+           width="100%">
+            <H5>NEWSLETTER</H5>
+          </div>
+        <div> <img src="https://images.ctfassets.net/wl6q2in9o7k3/21ifWUEOThoCNXNFlHKIlm/3c17b2662e16887877d3df1b82db9aa6/app.jpg" alt=""  height="80%"
+          width="100%">
+        <H5>GYMSHARK TRAINING</H5>
+      </div>
+    </div> 
+    </div>
+</div>
+<div id="pay_cards">
+    <div>
+        <div><img src="https://cdn4.iconfinder.com/data/icons/logos-and-brands/512/363_Visa_Credit_Card_logo-512.png" alt="" height="40px" width="50px"></div>
+        <div><img src="https://companiesmarketcap.com/img/company-logos/512/MA.png" alt="" height="40px" width="40px"></div>
+        <div><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/American_Express_logo_%282018%29.svg/1200px-American_Express_logo_%282018%29.svg.png" alt="" height="30px" width="40px"></div>
+        <div><img src="https://www.braintreepayments.com/images/logos/payment-methods/paypal-logo-block.svg" alt="" height="30px" width="40px"></div>
+        <div><img src="https://1000logos.net/wp-content/uploads/2020/04/Logo-Apple-Pay1.jpg" alt="" height="30px" width="40px"></div>
+        <div><img src="https://cdn.worldvectorlogo.com/logos/klarna.svg" alt="" height="30px" width="40px"></div>
+        <div><img src="https://trinity-core.s3.us-west-1.amazonaws.com/fortunly/2349/Square-to-Buy-Australian-Fintech-Afterpay-in-%2429-Billion-Deal---Featured-Image.png" alt="" height="30px" width="40px"></div>
+        
+    </div>
+
+
+    <div id="social_media">  <div><img src="https://cdn.logojoy.com/wp-content/uploads/20210422095037/discord-mascot.png" alt="" height="30px" width="35px"></div>
+    <div><img src="https://image.pngaaa.com/809/142809-middle.png" alt="" height="30px" width="40px"></div>
+    <div><img src="https://www.pngkit.com/png/full/785-7855464_app-for-youtube-opera-icon-png.png" alt="" height="30px" width="35px"></div>
+    <div><img src="https://toppng.com/uploads/preview/instagram-logo-circle-11549679754rhbcorxntv.png" alt="" height="30px" width="35px"></div>
+    <div><img src="https://e7.pngegg.com/pngimages/708/311/png-clipart-twitter-twitter-thumbnail.png" alt="" height="30px" width="35px"></div>
+    <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6zqSMqOsjFErvaSFvHI3k-Y8onsFjX7gt7nguIatopODmMjqn3xIDUv2EmO4TEG889WU&usqp=CAU" alt="" height="30px" width="40px"></div>
+  </div>
+</div>
+<div id="resorves">
+    <div><p>© 2022 | Gymshark Limited | All Rights Reserved | Be a visionary</p></div>
+    <div id="rules">
+        <a href="">Terms & Conditions</a>
+        <a href="">Terms of Use</a>
+        <a href=""> Privacy Notice</a>
+        <a href="">    Cookie Policy</a>
+        <a href="">Modern Slavery</a>
+    </div>
+</div>`
+}
+
+
+export default footer
